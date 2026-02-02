fffce05d084aabfb7492a4798dedcf572cd61568
Ashutosh Rai <ashurai9795@gmail.com>
2026-02-02 11:28:31 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,16 @@
+
+public class add {
+	public static void main(String[] args){
+		int a=7;
+		int b=8;
+		int c=0;
+		c=a+b;
+		System.out.println(c);
+		a=a^b;
+		b=a^b ;
+		a=a^b;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
